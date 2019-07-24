0acb436f4b2354f7afdb70b9093292a3886ddb65
Anon <A>
2019-07-24 17:08:44 +0300

    Created branch for CreateBugReport user story.

---
+++ RoadState/RoadState.Backend/Controllers/CreateBugReportController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoadState.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateBugReportController : Controller
+    {
+        [HttpPost()]
+        public async Task<IActionResult> CreateBugReport()
+            [FromBody] 
+        {
+
+            return Ok(_context.BugReports);
+        }
+    }
+}
---
+++ RoadState/RoadState.Backend/Models/CreateBugReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoadState.Backend.Models
+{
+    public class CreateBugReport
+    {
+        
+        public Location Location { get; set; }
+        public int ReliabilityLevel { get; set; }
+        public int ProblemLevel { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
