346a2e171b55fcb6f5cfb20fee1a4a88621d3ab8
Zhuravlov Dmitriy <box17776dima@gmail.com>
2019-07-28 21:14:50 +0300

    token + rate

---
+++ RoadState/RoadState.BusinessLayer/TransportModels/UserRateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoadState.BusinessLayer.TransportModels
+{
+    public class UserRateDTO
+    {
+        public int Id { get; set; }
+
+        public string Rate { get; set; }
+
+        public string Token { get; set; }
+    }
+}
