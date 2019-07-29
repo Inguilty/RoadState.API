3efb63160e430f00aa6faed49b6e54e29be74b93
Yurii Pohuliaiev <yurii.pohuliaiev@nure.ua>
2019-07-29 14:18:08 +0300

    Added request to Google Maps, setted a secret in appsettings.json

---
+++ RoadState/RoadState.Backend/Controllers/GeolocationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace RoadState.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeolocationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public GeolocationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private async Task<string> GetAddress(double latitude, double longitude)
+        {
+            string publicKey = _configuration["AppSettings:GoogleMapsAPIKey"];
+            string BASE_URL = $"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={publicKey}";
+            var client = new HttpClient();
+            var response = await client.GetAsync(BASE_URL);
+            if (response.StatusCode != HttpStatusCode.OK) return null;
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        [HttpGet("coords")]
+        public async Task<IActionResult> GetCurrentAddress([FromQuery] double longitude, double latitude)
+        {
+            string result = await GetAddress(latitude, longitude);
+            if (result is null) return Forbid("No access to Google Maps API");
+            return Ok(result);
+        }
+    }
+}
