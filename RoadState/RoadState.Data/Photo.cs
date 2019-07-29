f4046998fbb3439c401ef8e747d58484a6fa9aeb
Anon <A>
2019-07-30 00:38:50 +0300

    Restored DBcontext and Photo

--- RoadState/RoadState.Data/Photo.cs
+++ RoadState/RoadState.Data/Photo.cs
@@ -7,6 +7,7 @@ namespace RoadState.Data
     public class Photo : BaseEntity
     {
         public byte[] Blob { get; set; }
+
         public int BugReportId { get; set; }
         public BugReport BugReport { get; set; }
     }
